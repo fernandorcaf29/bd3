cb19a7b2b2cb025f7020eb84b66a3afdba3c3e0a
ItzOliver <ooliveralmeida@gmail.com>
2025-01-19 20:37:40 -0300

    Consulta 3 pagina 2

--- queries/page02/query03/query03.py
+++ queries/page02/query03/query03.py
@@ -11,7 +11,37 @@ spark = create_spark_session()
 ordered_segments_view = create_ordered_segments_view(spark)
 
 top_ten_segments = spark.sql(f"""
-    
+SELECT 
+    a."nome" AS "nome_aeroporto",
+    vf."cidade_origem",
+    vf."pais_origem",
+    vf."cidade_destino",
+    vf."pais_destino",
+    vf."qtdAtrasadosTotal",
+    vf."tempo_medio_acrescimo"
+FROM 
+    public."dAeroporto" AS a
+CROSS JOIN LATERAL (
+    SELECT
+        aOrig."Cidade" AS cidade_origem,
+        aOrig."Pais" AS pais_origem,
+        aDest."Cidade" AS cidade_destino,
+        aDest."Pais" AS pais_destino,
+        SUM(v."qtdAtrasados") AS "qtdAtrasadosTotal",
+        SUM(v."atrasoMinTotal") / NULLIF(SUM(v."qtdAtrasados"), 0) AS "tempo_medio_acrescimo"
+    FROM 
+        public."fVoo" AS v
+    INNER JOIN
+        public."dAeroporto" AS aDest ON v."idAeroDest" = aDest."id"
+    INNER JOIN
+        public."dAeroporto" AS aOrig ON v."idAeroOrig" = aOrig."id"
+    WHERE a."id" = aOrig."id"
+    GROUP BY 
+        aOrig."Cidade", aOrig."Pais", aDest."Cidade", aDest."Pais"
+) AS vf
+ORDER BY
+    vf."qtdAtrasadosTotal" DESC
+LIMIT 10;   
 """)
 
 top_ten_segments.rdd.saveAsTextFile("queries/page02/query03/output")
