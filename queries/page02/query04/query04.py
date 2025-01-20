c013eeda3e95ecb7416baff80db4901bdb42059b
Rafael Roza de Abreu Soares <rafaelabreu2003@gmail.com>
2025-01-20 00:57:15 -0300

    fix query 4 e 5 de pagina 2

--- queries/page02/query04/query04.py
+++ queries/page02/query04/query04.py
@@ -12,47 +12,40 @@ spark = create_spark_session()
 transform_tables_into_view(spark, ["dAeroporto", "fVoo"])
 
 top_ten_segments = spark.sql(
-f"""
+    f"""
+        SELECT 
+            a.nome AS nome_aeroporto,
+            vf.cidade_origem,
+            vf.pais_origem,
+            vf.cidade_destino,
+            vf.pais_destino,
+            vf.qtdCanceladosTotal
+        FROM 
+            dAeroporto AS a
+        JOIN LATERAL (
+            SELECT
+                aOrig.id AS id_origem,
+                aOrig.Cidade AS cidade_origem,
+                aOrig.Pais AS pais_origem,
+                aDest.Cidade AS cidade_destino,
+                aDest.Pais AS pais_destino,
+                SUM(v.qtdCancelados) AS qtdCanceladosTotal
+            FROM 
+                fVoo AS v
+            INNER JOIN
+                dAeroporto AS aDest ON v.idAeroDest = aDest.id
+            INNER JOIN
+                dAeroporto AS aOrig ON v.idAeroOrig = aOrig.id
+            GROUP BY 
+                aOrig.id, aOrig.Cidade, aOrig.Pais, aDest.Cidade, aDest.Pais
+        ) AS vf ON a.id = vf.id_origem
+        ORDER BY
+            vf.qtdCanceladosTotal DESC
+        LIMIT 10
+    """
+)
 
-""")
-
-top_ten_segments.rdd.saveAsTextFile("queries/page02/query04/output")
+output_dir = "queries/page02/query04/output"
+top_ten_segments.write.mode('overwrite').csv(output_dir, header=True)
 
 spark.stop()
-
-"""
-
-CONSULTA NÃO FUNCIONOU COM O SPARK MAS FUNCIONOU NO POSTGRES: ACCESSING_OUTER_QUERY_COLUMN_IS_NOT_ALLOWED
- 
-    SELECT
-        t.nome AS nome_aeroporto,
-        vf.cidade_origem,
-        vf.pais_origem,
-        vf.cidade_destino,
-        vf.pais_destino,
-        vf.qtdCanceladosTotal
-    FROM 
-        dAeroporto AS t
-    CROSS JOIN LATERAL (
-        SELECT
-            aOrig.Cidade AS cidade_origem,
-            aOrig.Pais AS pais_origem,
-            aDest.Cidade AS cidade_destino,
-            aDest.Pais AS pais_destino,
-            SUM(v.qtdCancelados) AS qtdCanceladosTotal
-        FROM 
-            fVoo AS v
-        INNER JOIN
-            dAeroporto AS aDest ON v.idAeroDest = aDest.id
-        INNER JOIN
-            dAeroporto AS aOrig ON v.idAeroOrig = aOrig.id
-        WHERE
-            t.id = aOrig.id
-        GROUP BY 
-            aOrig.Cidade, aOrig.Pais, aDest.Cidade, aDest.Pais
-        ORDER BY
-            qtdCanceladosTotal DESC
-        LIMIT 10
-    ) AS vf;
-
-"""
