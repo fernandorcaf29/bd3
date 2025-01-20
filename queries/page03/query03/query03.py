2397c73eb6a7bfb5dff3628c298c4d6bad50024a
Rafael Roza de Abreu Soares <rafaelabreu2003@gmail.com>
2025-01-20 11:35:56 -0300

    Update query03.py

--- queries/page03/query03/query03.py
+++ queries/page03/query03/query03.py
@@ -1,24 +1,37 @@
 import sys
 import os
+
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+from dto.readTable import transform_tables_into_view
 from config.sparkConfig import create_spark_session
-from utils.views import create_principais_justificativas_view
 
 # Quais as 10 principais justificativas para vôos cancelados
 
 spark = create_spark_session()
 
-principais_justificativas_view = create_principais_justificativas_view(spark)
+transform_tables_into_view(spark, ["fVooJustificativa", "dJustificativa"])
 
-top_ten = spark.sql(
+top_ten = spark.sql (
     f"""
-        SELECT * 
-        FROM {principais_justificativas_view}
+        SELECT 
+            j.cod AS justificativa,
+            COUNT(vj.idJustificativa) AS qtdCancelamentos
+        FROM 
+            fVooJustificativa AS vj
+        INNER JOIN 
+            dJustificativa AS j ON vj.idJustificativa = j.id
+        GROUP BY 
+            j.cod
+        ORDER BY 
+            qtdCancelamentos DESC
+        LIMIT 10;
     """
 )
 
 top_ten.show()
 
 top_ten.rdd.saveAsTextFile("queries/page03/query03/output")
+
 
 spark.stop()
